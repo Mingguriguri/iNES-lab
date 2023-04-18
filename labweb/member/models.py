d8ecfa3f0c16ce09d59c595a63217014b3fd7a6b
Kojungbeom <rhwndqja@icloud.com>
2023-04-18 13:50:18 +0900

    Complete member page

--- labweb/member/models.py
+++ labweb/member/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class member_list(models.Model):
     name_text = models.CharField(max_length=20)
+    one_sentence_text = models.CharField(max_length=50, default="Good, better, best")
     ID_photo_path_text = models.CharField(max_length=100)
+    second_photo_path = models.CharField(max_length=100, default="/static/brain.png")
+    
