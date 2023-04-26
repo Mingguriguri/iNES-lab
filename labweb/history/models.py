16e75312eee599f423f30d9e6c9e26166d30165f
Mingguriguri <rlaalswjd-@naver.com>
2023-04-26 14:01:39 +0900

    feedback 1 latest_Data2

--- labweb/history/models.py
+++ labweb/history/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class History(models.Model):
+    history_title = models.CharField(max_length=100)
+    history_dates = models.DateTimeField(blank = True, null = True)
+    history_photo_path = models.CharField(max_length=100, default="/static/brain.png")
+
+    
