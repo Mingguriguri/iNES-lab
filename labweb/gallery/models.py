a3b89056c72b92c0db35c9cf1695b9bed40a659d
Mingguriguri <rlaalswjd-@naver.com>
2023-04-26 13:55:30 +0900

    feedback 1 latest_data migration

--- labweb/gallery/models.py
+++ labweb/gallery/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Gallery(models.Model):
     photo_title = models.CharField(max_length=20)
+    photo_dates = models.DateTimeField(blank = True, null = True)
     photo_context = models.CharField(max_length=100)
     photo_path = models.CharField(max_length=100, default="/static/brain.png")
-    photo_dates = models.CharField(max_length=20)
 
     
--- labweb/projects/models.py
+++ labweb/projects/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class article_list(models.Model):
-    article_name = models.CharField(max_length=50)
-    article_keywords = models.CharField(max_length=50)
-    article_abstract = models.CharField(max_length=350)
+    article_name = models.CharField(max_length=150)
+    article_dates = models.DateTimeField(blank = True, null = True)
+    article_keywords = models.CharField(max_length=150)
+    article_abstract = models.CharField(max_length=1500)
     article_photo_path = models.CharField(max_length=100)
+
 
