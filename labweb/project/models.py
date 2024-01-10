c59b508a1b9445bcb239d5c36435618a92cc4f4d
Mingguriguri <rlaalswjd-@naver.com>
2024-01-10 21:37:35 +0900

    publicaton & projects : 최종 DB 수정

--- labweb/project/models.py
+++ labweb/project/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from member.models import member_list 
 from area.models import area
-from publication.models import Journal, Conference
-#from publications.models import Publication
 
 # Create your models here.
 class Projects(models.Model):
@@ -13,9 +11,7 @@ class Projects(models.Model):
     project_lead = models.ForeignKey(member_list, on_delete=models.CASCADE, related_name='project_lead')  # 프로젝트 리더
     members = models.ManyToManyField(member_list)  # 프로젝트에 참여하는 멤버들
     areas = models.ManyToManyField(area)  # 프로젝트 관련 영역
-    publications = models.ManyToManyField(Journal, blank=True)  # 관련된 저널 출판물
-    conferences = models.ManyToManyField(Conference, blank=True)  # 관련된 컨퍼런스 출판물
-    #publication = models.ForeignKey(Publication, on_delet=models.CASCADE)
+    
     def __str__(self):
         return self.name
 
--- labweb/publications/models.py
+++ labweb/publications/models.py
@@ -13,7 +13,7 @@ class Publication(models.Model):
     published_date = models.DateTimeField()
     doi_link = models.URLField()
     pub_type = models.CharField(max_length=4, choices=TYPE_CHOICES, default='JOUR')
-    related_project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    related_project = models.ForeignKey(Projects, on_delete=models.CASCADE, related_name='publications')
 
     def __str__(self):
         return self.name
