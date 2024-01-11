e16fe8d013d9250715c7d825c6a99a56652e6e1f
Mingguriguri <rlaalswjd-@naver.com>
2024-01-11 13:44:39 +0900

    update <Awards> DB

--- labweb/awards/models.py
+++ labweb/awards/models.py
@@ -4,7 +4,7 @@ from member.models import member_list
 import datetime
 # Create your models here.
 class Awards(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, default="")
     award_dates = models.DateTimeField(default=datetime.datetime.now)
     contents = models.TextField(blank=True, null=True)
     photo = models.FileField(upload_to="upload/%Y/%m/%d/", blank=True)
@@ -12,4 +12,4 @@ class Awards(models.Model):
     member = models.ManyToManyField(member_list)
 
     def __str__(self):
-        return self.awards_title
+        return self.title if self.title else "제목없음"
