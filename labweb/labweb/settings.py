a88e5f8da0961be1b79e1d5af138850653b2134a
Mingguriguri <rlaalswjd-@naver.com>
2024-02-06 15:49:58 +0900

    [Chore] ALLOWED_HOSTS 추가

--- labweb/labweb/settings.py
+++ labweb/labweb/settings.py
@@ -41,8 +41,9 @@ SECRET_KEY = "django-insecure-@r)b+$dd#k^#_e+blqmke+ig3dp-sx%3h2&chr$ey(#!2(79x6
 DEBUG = True
 
 ALLOWED_HOSTS = [
-           ".ap-northeast-2.compute.amazonaws.com"
-           #"127.0.0.1"
+           ".ines-lab.gachon.ac.kr",
+           ".ap-northeast-2.compute.amazonaws.com",
+           "127.0.0.1"
 ]
 
 # Application definition
