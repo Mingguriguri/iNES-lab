3d61c57afe54d39baf34383e483cf9d20233b7ff
Mingguriguri <rlaalswjd-@naver.com>
2024-04-12 13:08:32 +0900

    fix: 메인페이지 AI/HW 합치기

--- labweb/home/urls.py
+++ labweb/home/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('ver3/', views.ver3, name='ver3'),
+    path('', views.index, name='home'), # 현재 사용중인 버전
     # ex: /projects/name
 ]
--- labweb/home/views.py
+++ labweb/home/views.py
@@ -3,15 +3,9 @@ from django.template import loader
 
 
 
-def index(request):
+def index(request): # 현재사용중인 버전
     template = loader.get_template('../templates/labweb/homev2.html')
     context = {
     }
     return HttpResponse(template.render(context, request))
 
-
-def ver3(request):
-    template = loader.get_template('../templates/labweb/homev3.html')
-    context = {
-    }
-    return HttpResponse(template.render(context, request))
