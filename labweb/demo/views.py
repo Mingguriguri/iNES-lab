40ad5cc31cd65a7af0d886a90c7718e5db37b45a
Mingguriguri <rlaalswjd-@naver.com>
2024-04-12 13:12:12 +0900

    fix: Research Topic항목을 네비비게이션바에서 주석처리. 추후 HW팀 내용도 반영 예정

--- labweb/demo/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from django.template import loader
-
-def index(request):
-    template = loader.get_template('../templates/labweb/demo.html')
-    return HttpResponse(template.render({}, request))
