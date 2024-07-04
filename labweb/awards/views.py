314f591988133e3e7da7110bd6ee77c157ab6149
Mingguriguri <rlaalswjd-@naver.com>
2024-07-04 15:00:15 +0900

    #15-feat: [Award] 페이지에 ai, hw 뷰 컨트롤러 나누기

--- labweb/awards/views.py
+++ labweb/awards/views.py
@@ -5,9 +5,19 @@ from django.db.models.functions import ExtractYear
 from .models import Awards
 
 
-def index(request):
+def ai_award(request):
     awards = Awards.objects.all().order_by('-award_dates')
-    template = loader.get_template('../templates/labweb/awards.html')
+    awards = Awards.objects.filter(team='AI')
+    template = loader.get_template('../templates/labweb/award/ai_awards.html.html')
+    context = {
+        'award_list': awards
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def hw_award(request):
+    awards = Awards.objects.all().order_by('-award_dates')
+    template = loader.get_template('../templates/labweb/award/award/hw_awards.html.html')
     context = {
         'award_list': awards
     }
