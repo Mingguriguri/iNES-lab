4c1f677df30f95bf030c2c0eea895c8049e55493
Mingguriguri <rlaalswjd-@naver.com>
2024-07-04 16:11:53 +0900

    #15-refact: award view 파일의 HttpResponse 가독성이 좋은 render 함수로 수정

--- labweb/awards/views.py
+++ labweb/awards/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from django.db.models.functions import ExtractYear
 from .models import Awards
 
@@ -8,18 +6,15 @@ from .models import Awards
 def ai_award(request):
     awards = Awards.objects.all().order_by('-award_dates')
     awards = Awards.objects.filter(team='AI')
-    template = loader.get_template('../templates/labweb/award/ai_awards.html')
-    context = {
-        'award_list': awards
-    }
-    return HttpResponse(template.render(context, request))
-
+    return render(request, 'labweb/award/ai_awards.html',
+                  {
+                      'award_list': awards
+                  })
 
 def hw_award(request):
     awards = Awards.objects.all().order_by('-award_dates')
     awards = Awards.objects.filter(team='HW')
-    template = loader.get_template('../templates/labweb/award/hw_awards.html.html')
-    context = {
-        'award_list': awards
-    }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'labweb/award/hw_award.html',
+                  {
+                      'award_list': awards
+                  })
