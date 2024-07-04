ba1338f0f9563ad8291bd2387bcc4700b4b48137
Mingguriguri <rlaalswjd-@naver.com>
2024-07-04 16:24:43 +0900

    #15-feat: Ai, HW 엔드포인트로 분리하여 members  앱에 연결 + templates 정리

--- labweb/ai/urls.py
+++ labweb/ai/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from awards import views as awards_view
 from area import views as area_views
 from gallery import views as gallery_views
+from member import views as member_views
 
 urlpatterns = [
     path('awards/', awards_view.ai_award, name='ai_award'),
     path('research_topic/', area_views.ai_research_topic, name='ai_research_topic'),
     path('gallery/', gallery_views.ai_gallery, name='ai_gallery'),
-
+    path('members/', member_views.ai_members, name='ai_members'),
+    path('members/filter/', member_views.ai_filter_by_degree, name='ai_filter_by_degree'),
     # path('projects/', project_views.ai_projects, name='ai_projects'),
     # path('notices/', notice_views.ai_notices, name='ai_notices'),
 ]
--- labweb/hw/urls.py
+++ labweb/hw/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from awards import views as awards_view
 from area import views as area_views
 from gallery import views as gallery_views
+from member import views as member_views
 
 urlpatterns = [
     path('awards/', awards_view.hw_award, name='hw_award'),
     path('research_topic/', area_views.hw_research_topic, name='hw_research_topic'),
     path('gallery/', gallery_views.hw_gallery, name='hw_gallery'),
-
+    path('members/', member_views.hw_members, name='hw_members'),
+    path('members/filter/', member_views.hw_filter_by_degree, name='hw_filter_by_degree'),
     # path('projects/', project_views.ai_projects, name='ai_projects'),
     # path('notices/', notice_views.ai_notices, name='ai_notices'),
 ]
