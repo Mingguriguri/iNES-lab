615791ab9e11753df93c2bd9e8038430e37a7448
Mingguriguri <rlaalswjd-@naver.com>
2024-07-04 16:07:33 +0900

    #15-feat: Ai, HW 엔드포인트로 분리하여 gallery  앱에 연결 + templates 정리

--- labweb/ai/urls.py
+++ labweb/ai/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from awards import views as awards_view
 from area import views as area_views
+from gallery import views as gallery_views
 
 urlpatterns = [
     path('awards/', awards_view.ai_award, name='ai_award'),
     path('research_topic/', area_views.ai_research_topic, name='ai_research_topic'),
+    path('gallery/', gallery_views.ai_gallery, name='ai_gallery'),
 
     # path('projects/', project_views.ai_projects, name='ai_projects'),
     # path('notices/', notice_views.ai_notices, name='ai_notices'),
-    # path('gallery/', gallery_views.ai_gallery, name='ai_gallery'),
 ]
--- labweb/gallery/views.py
+++ labweb/gallery/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from .models import Gallery
 
-
-def index(request):
+def ai_gallery(request):
     photos = Gallery.objects.all().order_by('-upload_date')
+    photos = Gallery.objects.filter(team='AI')
+    return render(request, 'labweb/gallery/ai_gallery.html',
+                  {
+                      'Gallery': photos
+                  })
 
-    template = loader.get_template('../templates/labweb/gallery.html')
-    context = {
-            'Gallery': photos
-        }
-    return HttpResponse(template.render(context, request))
+def hw_gallery(request):
+    photos = Gallery.objects.all().order_by('-upload_date')
+    photos = Gallery.objects.filter(team='HW')
+    return render(request, 'labweb/gallery/hw_gallery.html',
+                  {
+                      'Gallery': photos
+                  })
