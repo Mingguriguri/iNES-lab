46131c42c1cf97304cd359c1317d43cb2bf2b2bb
Mingguriguri <rlaalswjd-@naver.com>
2024-07-06 00:19:02 +0900

    #17-fix: publications를 찾을 수 없는 에러가 발생하여 db데이터 삭제하고 새로 마이그레이션하여 해결

--- labweb/publications/views.py
+++ labweb/publications/views.py
@@ -2,51 +2,22 @@ from django.shortcuts import render
 from .models import Publication
 from django.db.models.functions import ExtractYear
 
-def ai_publications(request, pub_type):
+def get_publications_by_team(request, team, pub_type, template_name):
     # 논문 데이터 가져오기
-    publications = Publication.objects.filter(team='AI', pub_type=pub_type.upper()).order_by('-published_date')
-    
+    publications = Publication.objects.filter(team=team, pub_type=pub_type.upper()).order_by('-published_date')
+
     # 연도별로 정리
     years = publications.annotate(year=ExtractYear('published_date')).values_list('year', flat=True).distinct()
-    
-    # 저자 포맷팅
-    for pub in publications:
-        authors = pub.authors.all()
-        if authors.count() == 1:
-            pub.formatted_authors = authors.first().name
-        else:
-            author_names = [author.name for author in authors]
-            pub.formatted_authors = ', '.join(author_names[:-1]) + ' and ' + author_names[-1]
-    
+
     context = {
         'publications': publications,
         'years': years,
         'pub_type': pub_type,
     }
-    return render(request, 'labweb/publication/ai_publications.html', context)
+    return render(request, template_name, context)
 
-
+def ai_publications(request, pub_type):
+    return get_publications_by_team(request, 'AI', pub_type, 'labweb/publication/ai_publications.html')
 
 def hw_publications(request, pub_type):
-    # 논문 데이터 가져오기
-    publications = Publication.objects.filter(team='HW', pub_type=pub_type.upper()).order_by('-published_date')
-    
-    # 연도별로 정리
-    years = publications.annotate(year=ExtractYear('published_date')).values_list('year', flat=True).distinct()
-    
-    # 저자 포맷팅
-    for pub in publications:
-        authors = pub.authors.all()
-        if authors.count() == 1:
-            pub.formatted_authors = authors.first().name
-        else:
-            author_names = [author.name for author in authors]
-            pub.formatted_authors = ', '.join(author_names[:-1]) + ' and ' + author_names[-1]
-    
-    context = {
-        'publications': publications,
-        'years': years,
-        'pub_type': pub_type,
-    }
-    return render(request, 'labweb/publication/ai_publications.html', context)
-
+    return get_publications_by_team(request, 'HW', pub_type, 'labweb/publication/hw_publications.html')
