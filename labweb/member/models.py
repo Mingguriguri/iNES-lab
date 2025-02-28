9cbf5570665d7c15f67dc33dd3b89256fb6cf010
Minjeong Kim <101111603+Mingguriguri@users.noreply.github.com>
2025-02-28 23:46:33 +0900

    feat: 멤버리스트 모델의 email 필드를 nullable를 하도록 변경

--- labweb/member/models.py
+++ labweb/member/models.py
@@ -11,14 +11,14 @@ class member_list(models.Model):
         ('PD', 'Postdoc'),
         ('PHD', 'PhD'),
         ('MS', 'Master'),
-        ('BS', 'Bachelor'),      
+        ('BS', 'Bachelor'),
     ]
 
     STATUS_CHOICES = [
         ('CU', 'Current'),
         ('AL', 'Alumni'),
     ]
-    
+
     TEAM_CHOICES = (
         ('AI', 'AI Team'),
         ('HW', 'Hardware Team'),
@@ -27,13 +27,13 @@ class member_list(models.Model):
     team = models.CharField(max_length=2, choices=TEAM_CHOICES, default='HW')
 
     name = models.CharField(max_length=255, default="Name")
-    email = models.EmailField(max_length=254, default="@gachon.ac.kr")
+    email = models.EmailField(max_length=254, default="@gachon.ac.kr", blank=True, null=True)
     main_photo = models.FileField(upload_to="upload/%Y/%m/%d/", blank=True)
     hobby_photo = models.FileField(upload_to="upload/%Y/%m/%d/", blank=True)
-    motto = models.CharField(max_length=50, default="No data")
+    motto = models.CharField(max_length=50, blank=True, null=True)
     department = models.CharField(max_length=50, default = "Department of Biomedical Engineering")
     degree = models.CharField(max_length=50, choices=DEGREE_CHOICES, default='BS')
     status = models.CharField(max_length=2, choices=STATUS_CHOICES, default='CU')
-    
+
     def __str__(self):
         return self.name
